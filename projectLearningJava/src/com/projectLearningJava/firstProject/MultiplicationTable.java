a61eeeb5d8dc95f46bbca46a3f21840aa2162c34
kurtreyn <kurtreyn@yahoo.com>
2023-02-01 15:36:17 -0500

    updated MultiplicationTable

--- projectLearningJava/src/com/projectLearningJava/firstProject/MultiplicationTable.java
+++ projectLearningJava/src/com/projectLearningJava/firstProject/MultiplicationTable.java
@@ -8,6 +8,11 @@ public class MultiplicationTable {
 		}
 	}
 	
+	void print(int table, int from, int to) {
+		for(int i = from; i <= to; i++) {
+			System.out.printf("%d * %d = %d", table, i, table * i).println();
+		}
+	}
 	
 
 }
--- projectLearningJava/src/com/projectLearningJava/firstProject/MultiplicationTableRunner.java
+++ projectLearningJava/src/com/projectLearningJava/firstProject/MultiplicationTableRunner.java
@@ -4,7 +4,7 @@ public class MultiplicationTableRunner {
 
 	public static void main(String[] args) {
 		MultiplicationTable table = new MultiplicationTable();
-		table.print();
+		table.print(6, 1, 10);
 
 	}
 
