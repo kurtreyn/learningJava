70a9e81e28597752ec5622463f6cd11559d5384b
kurtreyn <kurtreyn@yahoo.com>
2023-02-01 15:18:58 -0500

    initial commit

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/29/609f341b6aa2001d1502cf6c34f6bce1
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author kurtreyn
+ *
+ */
+module projectLearningJava {
+}
---
+++ projectLearningJava/src/com/projectLearningJava/firstProject/MultiplicationTableRunner.java
@@ -0,0 +1,11 @@
+package com.projectLearningJava.firstProject;
+
+public class MultiplicationTableRunner {
+
+	public static void main(String[] args) {
+		MultiplicationTable table = new MultiplicationTable();
+		table.print();
+
+	}
+
+}
